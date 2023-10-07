cc7943f51a27e369092dab1ce25afb98ed1b844b
Tiago Silva <neiva_tiago@hotmail.com>
2023-10-07 21:13:55 +0100

    Angular View Child Decorator 2

--- angular-course-1-components/src/app/app.component.ts
+++ angular-course-1-components/src/app/app.component.ts
@@ -11,11 +11,23 @@ import { CourseCardComponent } from './course-card/course-card.component';
 export class AppComponent {
 
   coursesList = COURSES;
-
+  //Component
   @ViewChild(CourseCardComponent)
   card: CourseCardComponent;
+  //By template ref
+  @ViewChild('cardRef')
+  card2: CourseCardComponent;
+  //Get component in html element 
+  @ViewChild('cardRef2',{read: ElementRef} )
+  card3: ElementRef;
+  //Get html element 
+  @ViewChild('cardRef3')
+  card4: ElementRef;
 
   onCourseSelected(course: Course){
     console.log(this.card)
+    console.log(this.card2)
+    console.log(this.card3)
+    console.log(this.card4)
   }
 }
