80809b4e89e1bca563132e373924a438660c2070
Tiago Silva <neiva_tiago@hotmail.com>
2023-10-10 21:39:31 +0100

    Angular ViewChildren Decorator and Query List

--- angular-course-1-components/src/app/app.component.ts
+++ angular-course-1-components/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import {COURSES} from '../db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
@@ -12,20 +12,34 @@ export class AppComponent implements AfterViewInit {
 
   coursesList = COURSES;
   //Component
-  @ViewChild('cardRef')
-  card: CourseCardComponent;
-  @ViewChild('container')
-  containerDid: ElementRef;
+  @ViewChildren(CourseCardComponent)
+  cards: QueryList<CourseCardComponent>;
+
 
   constructor(){
   }
 
   ngAfterViewInit() {
-    console.log("Container Div", this.card)
+
+    this.cards.changes.subscribe(
+      cards => console.log(cards)
+    );
+  }
+
+  onCoursesEdited(){
+    this.coursesList.push(
+      {
+        id: 11,
+        description: "Angular 2",
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+        category: 'INTERMEDIATE',
+        lessonsCount: 10
+      }
+    );
   }
 
   onCourseSelected(course: Course){
-    console.log(this.card)
-
+    console.log(course)
   }
 }
