bd82d95bf794148bf9cee9cab66547b9070215b7
Tiago Silva <neiva_tiago@hotmail.com>
2023-10-10 22:04:39 +0100

    Angular Content Projection with ng-content

--- angular-course-1-components/src/app/app.component.ts
+++ angular-course-1-components/src/app/app.component.ts
@@ -11,32 +11,12 @@ import { CourseCardComponent } from './course-card/course-card.component';
 export class AppComponent implements AfterViewInit {
 
   coursesList = COURSES;
-  //Component
-  @ViewChildren(CourseCardComponent)
-  cards: QueryList<CourseCardComponent>;
-
 
   constructor(){
   }
 
   ngAfterViewInit() {
 
-    this.cards.changes.subscribe(
-      cards => console.log(cards)
-    );
-  }
-
-  onCoursesEdited(){
-    this.coursesList.push(
-      {
-        id: 11,
-        description: "Angular 2",
-        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
-        longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
-        category: 'INTERMEDIATE',
-        lessonsCount: 10
-      }
-    );
   }
 
   onCourseSelected(course: Course){
